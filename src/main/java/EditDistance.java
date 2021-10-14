16cb021d2a68053b4a834395ee01de9eb3d269a7
ARTAUD Cyril <cyril.artaud@etu.univ-amu.fr>
2021-10-14 16:01:30 +0200

    creation des methodes de EditDistance

--- src/main/java/EditDistance.java
+++ src/main/java/EditDistance.java
@@ -1,2 +1,15 @@
 public class EditDistance {
+
+    public int distanceBetween(String word, String comparedWord) {
+        int distance = 0;
+        distance = distanceBetween(word, comparedWord, distance);
+        return distance;
+    }
+
+    private int distanceBetween(String word, String comparedWord, int distance) {
+        if (word.charAt(word.length() - 1) == comparedWord.charAt(comparedWord.length() - 1))
+            distance = distanceBetween(word.substring(0, word.length() - 2),
+                    comparedWord.substring(0, comparedWord.length() - 2), distance);
+        return distance;
+    }
 }
